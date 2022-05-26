908b8170ade2524e66459628ecf29006fefb0529
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-05-25 20:23:13 -0500

    1

---
+++ deportista.py
@@ -0,0 +1,16 @@
+class Deportista:
+    def __init__(self, deporte, añosPracticados):
+        self._deporte = deporte
+        self._añosPracticados = añosPracticados
+
+    def getDeporte(self):
+        return self._Deporte
+
+    def getAñosPracticados(self):
+        return self._añosPracticados
+
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def setAñosPracticados(self, añosPracticados):
+        self._añosPracticados = añosPracticados
---
+++ futbolista.py
@@ -0,0 +1,42 @@
+from deportista import Deportista
+from persona import Persona
+
+class Futbolista(Persona, Deportista):
+    listaFutbolistas = []
+    def __init__(self, nombre, edad, altura, sexo, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+        Futbolista.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, "Futbol", añosPracticando)
+        self._golesMarcados = golesMarcados
+        self._tarjetasRojas = tarjetasRojas
+        self._piernaHabil = piernaHabil
+        Futbolista.listaFutbolistas.append(self)
+
+    @staticmethod
+    def  getListaFutbolistas() :
+        return Futbolista.listaFutbolistas
+
+    @staticmethod
+    def setListaFutbolistas( listaFutbolistas) :
+        Futbolista.listaFutbolistas = listaFutbolistas
+
+    def  getGolesMarcados(self) :
+        return self._golesMarcados
+
+    def setGolesMarcados(self, _golesMarcados) :
+        self._golesMarcados = _golesMarcados
+
+    def  getTarjetasRojas(self) :
+        return self._tarjetasRojas
+
+    def set_tarjetasRojas(self, _tarjetasRojas) :
+        self._tarjetasRojas = _tarjetasRojas
+
+    def  getPiernaHabil(self) :
+        return self._piernaHabil
+
+    def setPiernaHabil(self, _piernaHabil) :
+        self._piernaHabil = _piernaHabil
+
+futbolista = Futbolista("Juan Pablo", 30, "1,80", "M", 12, 400, 1, "Derecha")
+print(isinstance(futbolista, Deportista) and isinstance(futbolista, Persona))
+
