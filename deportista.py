97f01f22831448369eb61f6959e16861fce46318
Miguel Barrera <59462086+MiguelBa1@users.noreply.github.com>
2022-05-25 20:29:10 -0500

    1

--- deportista.py
+++ deportista.py
@@ -1,16 +1,16 @@
 class Deportista:
-    def __init__(self, deporte, añosPracticados):
+    def __init__(self, deporte, añosPracticando):
         self._deporte = deporte
-        self._añosPracticados = añosPracticados
+        self._añosPracticados = añosPracticando
 
     def getDeporte(self):
         return self._Deporte
 
-    def getAñosPracticados(self):
-        return self._añosPracticados
+    def getAñosPracticando(self):
+        return self._añosPracticandos
 
     def setDeporte(self, deporte):
         self._deporte = deporte
 
-    def setAñosPracticados(self, añosPracticados):
-        self._añosPracticados = añosPracticados
+    def setAñosPracticados(self, añosPracticando):
+        self._añosPracticados = añosPracticando
